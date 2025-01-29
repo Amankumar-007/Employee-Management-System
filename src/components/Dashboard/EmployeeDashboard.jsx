b1441c8ce865fc2b39447af4991f9c0e1a037d5b
Amankumar-007 <amanr3388@gmail.com>
2025-01-29 09:19:23 +0530

    employee dashboard added

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,9 +1,15 @@
 import Header from "../other/Header"
+import TaskNumber from "../other/TaskNumber";
+import TaskList from "../TaskList/TaskList";
+
 
 const EmployeeDashboard = () => {
   return (
     <>
     <Header/>
+    <TaskNumber/>
+    <TaskList/>
+    
     </>
   )
 }
